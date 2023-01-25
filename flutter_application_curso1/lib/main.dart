99aa813dd32a6432b244034871b93976fda34196
Mauricio <mauricio@rodosoft.com.br>
2023-01-25 09:17:48 -0300

    continuando curso 1 do alura de flutter

--- flutter_application_curso1/lib/main.dart
+++ flutter_application_curso1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,32 +15,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: 
-            [Container(color: Colors.red, width: 100, height: 100,), 
-            Container(color: Colors.blue, width: 50, height: 50,)],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: 
-            [Container(color: Colors.blue, width: 100, height: 100,), 
-            Container(color: Colors.red, width: 50, height: 50,)],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-            Container(color: Colors.cyan,height: 50,width: 50,),
-            Container(color: Colors.pinkAccent,height: 50,width: 50,),
-            Container(color: Colors.purple,height: 50,width: 50,),
-          ],)
-        ]
-      ,),
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: 
+              [Container(color: Colors.red, width: 100, height: 100,), 
+              Container(color: Colors.blue, width: 50, height: 50,)],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: 
+              [Container(color: Colors.blue, width: 100, height: 100,), 
+              Container(color: Colors.red, width: 50, height: 50,)],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              Container(color: Colors.cyan,height: 50,width: 50,),
+              Container(color: Colors.pinkAccent,height: 50,width: 50,),
+              Container(color: Colors.purple,height: 50,width: 50,),
+            ],),
+            Container(
+              color: Colors.amber,height: 30,width: double.infinity,
+              child: 
+                Text('Teste texto',style: TextStyle(color: Colors.black, fontSize: 28,decoration: TextDecoration.none,),textAlign: TextAlign.center,)
+            ,),
+            ElevatedButton(
+              onPressed: (){print('você apertou o botão');}, 
+              child: Text('Aperte o botão!')
+            ,),
+          ]
+        ,),
+      ),
     );
   }
 }
