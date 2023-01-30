d395c2e21dc48938946bddb02c5c509a1d6efe10
Mauricio <mauricio@rodosoft.com.br>
2023-01-30 10:58:45 -0300

    Continuando curso 1 do alura de flutter

--- flutter_application_curso1/lib/main.dart
+++ flutter_application_curso1/lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,43 +23,172 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text('Tarefas'),),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Tarefas('Aprender flutter'),
-            Tarefas('Andar de bike'),
-            Tarefas('Jogar the witcher'),
-          ],
+        appBar: AppBar(
+          leading: Container(),
+          title: Text('Tarefas'),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: (){},),
-      )
-    ,);
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 200),
+          child: ListView(
+            children: [
+              Tarefas('Aprender flutter no café da manhã', 'content/img/estudar.png', 3),
+              Tarefas('Andar de bike', 'content/img/andarbike.jpg', 2),
+              Tarefas('Jogar dark souls', 'content/img/jogarvideogame.jpg', 5),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
+        ),
+      ),
+    );
   }
 }
 
-class Tarefas extends StatelessWidget {
+class Tarefas extends StatefulWidget {
   final String nome;
+  final String foto;
 
-  const Tarefas(this.nome,{ Key? key }) : super(key: key);
+  final int dificuldade;
+  int dificuldadeMaxima = 5;
+
+  Tarefas(this.nome, this.foto, this.dificuldade, {Key? key}) : super(key: key);
+
+  @override
+  State<Tarefas> createState() => _TarefasState();
+}
+
+class _TarefasState extends State<Tarefas> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(color: Colors.blue, height: 140,),
-          Container(color: Colors.white, height: 100, child: 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
+              height: 140,
+            ),
+            Column(
               children: [
-                Container(color: Colors.black26,width: 72,height: 100,),
-                Text(nome),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: Colors.black26),
+                        width: 72,
+                        height: 100,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4),
+                            child: Image.asset(
+                              widget.foto,
+                              fit: BoxFit.cover,
+                            )),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: 200,
+                              child: Text(
+                                widget.nome,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    overflow: TextOverflow.ellipsis),
+                              )),
+                          Row(
+                            children: [
+                              for (int cont = 0;
+                                  cont < widget.dificuldade;
+                                  cont++)
+                                Icon(
+                                  Icons.star,
+                                  size: 15,
+                                  color: Colors.blue,
+                                ),
+                              for (int cont = 0;
+                                  cont <
+                                      widget.dificuldadeMaxima -
+                                          widget.dificuldade;
+                                  cont++)
+                                Icon(
+                                  Icons.star,
+                                  size: 15,
+                                  color: Colors.blue[100],
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text(
+                                  "UP",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          width: 200,
+                          child: LinearProgressIndicator(
+                            color: Colors.white,
+                            value: widget.dificuldade > 0
+                                ? (nivel / widget.dificuldade) / 10
+                                : 1,
+                          )),
+                      Text(
+                        'Nivel: ${nivel}',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
