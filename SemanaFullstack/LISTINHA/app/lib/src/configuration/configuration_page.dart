92ead15e7930071c48b7f797e20cd221c5fc9b73
Mauricio <mauricio@rodosoft.com.br>
2023-03-01 08:51:49 -0300

    Continuando curso da semana fullstack de flutter. Feito refatoração do código e iniciado gerenciamente de estado

--- SemanaFullstack/LISTINHA/app/lib/src/configuration/configuration_page.dart
+++ SemanaFullstack/LISTINHA/app/lib/src/configuration/configuration_page.dart
@@ -11,7 +11,67 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('LISTINHA'),),
+      appBar: AppBar(
+        title: Text('LISTINHA'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          right: 20,
+          left: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text('Sistema'),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text('Claro'),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+              title: const Text('Escuro'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Controle de dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Apagar cache e reiniciar o app'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
